12392707abe55113a59f02f925c56f23cd5be46c
root <matthewmontgomery2163@aol.com git config --global user.name Matthew21216363clearcdcdgit add .git commit -m wikgit config --global user.email matthewmontgomery2163@aol.com>
2023-09-07 20:15:57 -0700

    Zik

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-
+from sys import argv
+if __name__ == '__main__':
     total = 0
-    for i in range(1, len(sys.argv)):
-        total += int(sys.argv[i])
-    print("{}".format(total))
+    for i in range(1, len(argv)):
+        total += int(argv[i])
+    print(f"{total}")
